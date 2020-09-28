5a2520bbfcaf21ca018becb2a65238094f0a70f7
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-09-28 15:24:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  separator = "|"
-  lines = "-----------"
-
-  puts "#{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
-  puts "#{lines}"
-  puts "#{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
-  puts "#{lines}"
-  puts "#{board[6]} #{separator} #{board[7]} #{separator} #{board[8]}"
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
   # board = ["o", " x", " ", " ", "x ", " ", " x", " o", " "]
